556a2c74fe3406b006b0c5d0876d60afb82381fa
Rohan Singh Rajput <43549381+rohan-rajput@users.noreply.github.com>
2021-03-26 10:46:23 +0530

    bug fixes with new file
    wrong latency space issue  and bug fixing done here.

---
+++ keras-mnist-VAE.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+
+import tensorflow as tf
+from tensorflow.keras.layers import Dense,Lambda,Input
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras import backend as K
+from tensorflow.keras import Model
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+import os
+import gzip
+from tensorflow.keras.utils import get_file
+
+
+
+
+
+def load_data():
+    """Loads the Kannada-MNIST dataset.
+     Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    """
+    dirname = os.path.join('datasets', 'Kannada-mnist')
+    base = 'https://github.com/vinayprabhu/Kannada_MNIST/blob/master/data/output_tensors/MNIST_format/'
+    files = ['y_kannada_MNIST_train-idx1-ubyte.gz', 'X_kannada_MNIST_train-idx3-ubyte.gz',
+             'y_kannada_MNIST_test-idx1-ubyte.gz', 'X_kannada_MNIST_test-idx3-ubyte.gz']
+
+    paths = []
+    for fname in files:
+        paths.append(get_file(fname,
+                              origin=base + fname+'?raw=true',
+                              cache_subdir=dirname))
+
+    with gzip.open(paths[0], 'rb') as lbpath:
+        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+
+    with gzip.open(paths[1], 'rb') as imgpath:
+        x_train = np.frombuffer(imgpath.read(), np.uint8,
+                                offset=16).reshape(len(y_train), 28, 28)
+
+    with gzip.open(paths[2], 'rb') as lbpath:
+        y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+
+    with gzip.open(paths[3], 'rb') as imgpath:
+        x_test = np.frombuffer(imgpath.read(), np.uint8,
+                               offset=16).reshape(len(y_test), 28, 28)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+
+
+
+# data load
+(x_tr, y_tr), (x_te, y_te) = load_data()
+x_tr, x_te = x_tr.astype('float32')/255., x_te.astype('float32')/255.
+x_tr, x_te = x_tr.reshape(x_tr.shape[0], -1), x_te.reshape(x_te.shape[0], -1)
+print(x_tr.shape, x_te.shape)
+
+# network parameters
+batch_size, n_epoch = 100, 100
+n_hidden, z_dim = 256, 2
+
+
+
+
+
+# encoder
+x = Input(shape=(x_tr.shape[1:]))
+x_encoded = Dense(n_hidden, activation='relu')(x)
+x_encoded = Dense(n_hidden//2, activation='relu')(x_encoded)
+
+mu = Dense(z_dim)(x_encoded)
+log_var = Dense(z_dim)(x_encoded)
+
+
+
+
+
+# sampling function
+def sampling(args):
+    mu, log_var = args
+    eps = K.random_normal(shape=(batch_size, z_dim), mean=0., stddev=1.0)
+    return mu + K.exp(log_var) * eps
+
+z = Lambda(sampling, output_shape=(z_dim,))([mu, log_var])
+
+
+
+
+
+# decoder
+z_decoder1 = Dense(n_hidden//2, activation='relu')
+z_decoder2 = Dense(n_hidden, activation='relu')
+y_decoder = Dense(x_tr.shape[1], activation='sigmoid')
+
+z_decoded = z_decoder1(z)
+z_decoded = z_decoder2(z_decoded)
+y = y_decoder(z_decoded)
+
+
+# loss
+bce = tf.keras.losses.BinaryCrossentropy()
+reconstruction_loss = bce(x, y) * x_tr.shape[1]
+kl_loss = 0.5 * K.sum(K.square(mu) + K.exp(log_var) - log_var - 1, axis = -1)
+vae_loss = reconstruction_loss + kl_loss
+
+# build model
+vae = Model(x, y)
+vae.add_loss(vae_loss)
+vae.compile(optimizer='rmsprop')
+vae.summary()
+
+
+
+
+
+# train
+vae.fit(x_tr,
+       shuffle=True,
+       epochs=n_epoch,
+       batch_size=batch_size,
+       validation_data=(x_te, None), verbose=1)
+
+
+
+
+
+# build encoder
+encoder = Model(x, mu)
+encoder.summary()
+
+
+
+
+
+# Plot of the digit classes in the latent space
+x_te_latent = encoder.predict(x_te, batch_size=batch_size)
+plt.figure(figsize=(6, 6))
+plt.scatter(x_te_latent[:, 0], x_te_latent[:, 1], c=y_te)
+plt.colorbar()
+plt.show()
+
+
+
+
+# build decoder
+decoder_input = Input(shape=(z_dim,))
+_z_decoded = z_decoder1(decoder_input)
+_z_decoded = z_decoder2(_z_decoded)
+_y = y_decoder(_z_decoded)
+generator = Model(decoder_input, _y)
+generator.summary()
+
+# display a 2D manifold of the digits
+from scipy.stats import norm
+n = 15 # figure with 15x15 digits
+digit_size = 28
+figure = np.zeros((digit_size * n, digit_size * n))
+
+grid_x = norm.ppf(np.linspace(0.05, 0.95, n)) 
+grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
+
+for i, yi in enumerate(grid_x):
+    for j, xi in enumerate(grid_y):
+        z_sample = np.array([[xi, yi]])
+        x_decoded = generator.predict(z_sample)
+        digit = x_decoded[0].reshape(digit_size, digit_size)
+        figure[i * digit_size: (i + 1) * digit_size,
+               j * digit_size: (j + 1) * digit_size] = digit
+
+plt.figure(figsize=(10, 10))
+plt.imshow(figure, cmap='gist_gry_r')
+plt.show()
+
+
+
+
+
